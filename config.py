73bcd456c91e6692a1d07aa177faacbb7c2dd960
Andrey Okatev <okatev87@yandex.ru>
2025-05-27 22:46:45 +0300

    add tests

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+context = os.getenv('context', 'bstack')
+bstack_userName = os.getenv('bstack_userName', 'bsuser_x5aNTP')
+bstack_accessKey = os.getenv('bstack_accessKey', 'AsWqAMCwqiyCmKtmvTNi')
