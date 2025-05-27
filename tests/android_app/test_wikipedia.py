09d86e7a809ac2465634ac619235feb6f96b206f
Andrey Okatev <okatev87@yandex.ru>
2025-05-28 00:17:52 +0300

    add second test

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -4,7 +4,6 @@ from selene import browser, have
 
 
 def test_search():
-
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
@@ -13,3 +12,13 @@ def test_search():
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
+
+
+def test_click_by_article():
+    with step('Type search'):
+        browser.element((AppiumBy.CLASS_NAME, "android.widget.ImageView")).click()
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.CLASS_NAME, 'android.widget.TextView'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('EN'))
